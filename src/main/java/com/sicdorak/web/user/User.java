4ecd1ebdb2131f89c774ebe2b722c60117f93e89
ekdus04 <dygim04@gmail.com>
2025-08-08 18:21:05 +0900

    [feat] User entity Getter, Builder 추가 / 필드 제약조건 추가 및 변경

--- src/main/java/com/sicdorak/web/user/User.java
+++ src/main/java/com/sicdorak/web/user/User.java
@@ -1,24 +1,32 @@
 package com.sicdorak.web.user;
 
 import jakarta.persistence.*;
+import lombok.*;
+
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 30)
     private String name;
     @Column(nullable = false, unique = true)
     private String email;
     @Column(nullable = false)
     private String password;
 
-    private String kakaoId;
+    private Integer kakaoId;
 
+    @Column(columnDefinition = "TEXT")
     private String profileImage;
+
     @Column(nullable = false)
     private boolean allowNotify = false;
     private LocalDateTime createTime = LocalDateTime.now();
@@ -27,7 +35,7 @@ public class User {
     @Column(nullable = false)
     private AccessLevel accessLevel = AccessLevel.USER;
 
-    public static enum AccessLevel {
+    public enum AccessLevel {
         ADMIN,
         USER
     }
