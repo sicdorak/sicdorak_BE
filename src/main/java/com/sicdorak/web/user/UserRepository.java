9edb97b5c29cebd6505d38411cf52a054511eb92
ekdus04 <dygim04@gmail.com>
2025-08-22 21:13:50 +0900

    feat: findByEmail 구현

--- src/main/java/com/sicdorak/web/user/UserRepository.java
+++ src/main/java/com/sicdorak/web/user/UserRepository.java
@@ -1,6 +1,10 @@
 package com.sicdorak.web.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    // AuthService에서 email로 가입된 계정 유무 확인에 사용
+    // Optional = null 체크를 더 안전하게 하기 위한 도구
+    Optional<User> findByEmail(String email);
 }
